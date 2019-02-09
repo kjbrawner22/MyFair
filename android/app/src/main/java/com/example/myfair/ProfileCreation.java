a8d87f654e41c60457d141540575f8854ed0e64f
Joshua Helms <jghelms@crimson.ua.edu>
2019-02-09 16:08:53 -0600

    restructured buttons, added uID

--- android/app/src/main/java/com/example/myfair/ProfileCreation.java
+++ android/app/src/main/java/com/example/myfair/ProfileCreation.java
@@ -3,10 +3,19 @@ package com.example.myfair;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfileCreation extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileCreation extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private FirebaseUser user = mAuth.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +23,23 @@ public class ProfileCreation extends AppCompatActivity {
         setContentView(R.layout.activity_profile_creation);
 
         Button btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button btnUpdateInfo = findViewById(R.id.btnUpdateInfo);
+        btnBack.setOnClickListener(this);
+        btnUpdateInfo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btnBack:
                 Intent intent = new Intent(ProfileCreation.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        });
+                break;
+            case R.id.btnUpdateInfo:
+                Toast.makeText(this, "" + user.getUid(), Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
