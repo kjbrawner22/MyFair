c71817d73c5c0845060104ddf79088c79f605556
Kenny Brawner <kjbrawner22@gmail.com>
2019-02-09 16:03:50 -0600

    blank User class

---
+++ android/app/src/main/java/User.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+    public static final String TYPE_STUDENT = "student";
+    public static final String TYPE_RECRUITER = "recruiter";
+    public static final String TYPE_OWNER = "owner";
+
+    private String type;
+
+    private String firstName;
+    private String lastName;
+
+    public User() {
+
+    }
+}
