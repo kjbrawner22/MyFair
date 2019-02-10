a0b1a416134353695e3191a711feedbcd8fa01b0
Joshua Helms <jghelms@crimson.ua.edu>
2019-02-09 21:15:00 -0600

    data adds to database using user class

--- android/app/src/main/java/com/example/myfair/ProfileCreation.java
+++ android/app/src/main/java/com/example/myfair/ProfileCreation.java
@@ -1,6 +1,7 @@
 package com.example.myfair;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,18 +10,30 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileCreation extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
     private FirebaseUser user = mAuth.getCurrentUser();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String TAG = "ProfileCreation";
+    private User usrObj = new User("student", "Josh", "Helms");
+    private TextView txtFname, txtLname, txtType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation);
+
+        txtFname = findViewById(R.id.txtFname);
+        txtLname = findViewById(R.id.txtLname);
+        txtType = findViewById(R.id.txtType);
 
         Button btnBack = findViewById(R.id.btnBack);
         Button btnUpdateInfo = findViewById(R.id.btnUpdateInfo);
@@ -38,8 +51,31 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
                 finish();
                 break;
             case R.id.btnUpdateInfo:
+                usrObj.setFirstName(txtFname.getText().toString());
+                usrObj.setLastName(txtLname.getText().toString());
+                usrObj.setType(txtType.getText().toString());
+                updateUser();
                 Toast.makeText(this, "" + user.getUid(), Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
+    private void updateUser() {
+        DocumentReference docRef = db.collection("users").document(user.getUid());
+        docRef.set(usrObj)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
+
+    }
+
 }
