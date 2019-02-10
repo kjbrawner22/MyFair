3fd1b4ad6589ced0fd7a07dd94af940a85b61465
Joshua Helms <jghelms@crimson.ua.edu>
2019-02-10 16:05:12 -0600

    added type spinner selection, moved user classes to correct folder, implemented ProfileCreation to use worker/student instead of just user

--- android/app/src/main/java/com/example/myfair/ProfileCreation.java
+++ android/app/src/main/java/com/example/myfair/ProfileCreation.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,14 +19,17 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileCreation extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
     private FirebaseUser user = mAuth.getCurrentUser();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final String TAG = "ProfileCreation";
-    private User2 usrObj = new User2("student", "Josh", "Helms");
-    private TextView txtFname, txtLname, txtType;
+    private TextView txtFname, txtLname;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +38,15 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
 
         txtFname = findViewById(R.id.txtFname);
         txtLname = findViewById(R.id.txtLname);
-        txtType = findViewById(R.id.txtType);
+
+        addItemsOnSpinner();
+        addSpinnerListener();
 
         Button btnBack = findViewById(R.id.btnBack);
         Button btnUpdateInfo = findViewById(R.id.btnUpdateInfo);
         btnBack.setOnClickListener(this);
         btnUpdateInfo.setOnClickListener(this);
     }
-
     @Override
     public void onClick(final View v) {
         int id = v.getId();
@@ -51,16 +57,46 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
                 finish();
                 break;
             case R.id.btnUpdateInfo:
-                usrObj.setFirstName(txtFname.getText().toString());
-                usrObj.setLastName(txtLname.getText().toString());
-                usrObj.setType(txtType.getText().toString());
-                updateUser();
-                Toast.makeText(this, "" + user.getUid(), Toast.LENGTH_SHORT).show();
+                if(validProfileFields()) {
+                    updateClickRoutine();
+                    break;
+                }
+                Toast.makeText(this, "" + "Fill required fields.", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
 
-    private void updateUser() {
+    public void updateClickRoutine(){
+        spinner = (Spinner) findViewById(R.id.spinner);
+        String strType = spinner.getSelectedItem().toString();
+        if(strType.equals("Student")){
+            Student studentObj = new Student(txtFname.getText().toString(), txtLname.getText().toString(),"","","");
+            updateUser(studentObj);
+        }
+        else {
+            Worker workerObj = new Worker(txtFname.getText().toString(), txtLname.getText().toString(), "", "");
+            updateUser(workerObj);
+        }
+    }
+
+    public void addItemsOnSpinner() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("Student");
+        list.add("Recruiter");
+        list.add("Employer");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void addSpinnerListener() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new OnItemSelectedCustom());
+    }
+
+    private void updateUser(User usrObj) {
         DocumentReference docRef = db.collection("users").document(user.getUid());
         docRef.set(usrObj)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -76,6 +112,30 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
                     }
                 });
 
+    }
+
+    private boolean validProfileFields() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        final String fname = txtFname.getText().toString();
+        final String lname = txtLname.getText().toString();
+        final String usrType = spinner.getSelectedItem().toString();
+
+        if (fname.isEmpty()) {
+            txtFname.setError("First name field is required.");
+            txtFname.requestFocus();
+        } else if (lname.isEmpty()) {
+            txtLname.setError("Last name field is required.");
+            txtLname.requestFocus();
+        } else if (usrType.isEmpty()) {
+            spinner.setPrompt("Select item");
+            spinner.requestFocus();
+        }
+        else {
+            return true;
+        }
+
+        return false;
     }
 
 }
---
+++ android/app/src/main/java/com/example/myfair/Worker.java
@@ -0,0 +1,32 @@
+package com.example.myfair;
+
+public class Worker extends User {
+    private String companyName;
+    private String jobTitle;
+
+    public Worker() {
+        super(User.TYPE_WORKER);
+    }
+
+    public Worker(String firstName, String lastName, String companyName, String jobTitle) {
+        super(User.TYPE_WORKER, firstName, lastName);
+        this.companyName = companyName;
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
