854c63599a14afd18f63b3d1a4f5cacb0ed8858f
Kenny Brawner <kjbrawner22@gmail.com>
2019-02-10 18:50:29 -0600

    User class structure changed, some layout mods

--- android/app/src/main/java/com/example/myfair/ProfileCreation.java
+++ android/app/src/main/java/com/example/myfair/ProfileCreation.java
@@ -6,41 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ProfileCreation extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "ProfileCreation";
 
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
-    private FirebaseUser user = mAuth.getCurrentUser();
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private final String TAG = "ProfileCreation";
-    private TextView txtFname, txtLname;
-    private Spinner spinner;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseFirestore db;
+    private EditText etFname, etLname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_creation);
 
-        txtFname = findViewById(R.id.txtFname);
-        txtLname = findViewById(R.id.txtLname);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
 
-        addItemsOnSpinner();
-        addSpinnerListener();
+        etFname = findViewById(R.id.etFname);
+        etLname = findViewById(R.id.etLname);
 
         Button btnBack = findViewById(R.id.btnBack);
         Button btnUpdateInfo = findViewById(R.id.btnUpdateInfo);
@@ -58,7 +55,7 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.btnUpdateInfo:
                 if(validProfileFields()) {
-                    updateClickRoutine();
+                    updateProfile();
                     break;
                 }
                 Toast.makeText(this, "" + "Fill required fields.", Toast.LENGTH_SHORT).show();
@@ -66,72 +63,42 @@ public class ProfileCreation extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    public void updateClickRoutine(){
-        spinner = (Spinner) findViewById(R.id.spinner);
-        String strType = spinner.getSelectedItem().toString();
-        if(strType.equals("Student")){
-            Student studentObj = new Student(txtFname.getText().toString(), txtLname.getText().toString(),"","","");
-            updateUser(studentObj);
-        }
-        else {
-            Worker workerObj = new Worker(txtFname.getText().toString(), txtLname.getText().toString(), "", "");
-            updateUser(workerObj);
-        }
-    }
+    public void updateProfile(){
+        final String firstName = etFname.getText().toString();
+        final String lastName = etLname.getText().toString();
+        // final String universityName = etUniversityName.getText().toString();
 
-    public void addItemsOnSpinner() {
-        spinner = (Spinner) findViewById(R.id.spinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Student");
-        list.add("Recruiter");
-        list.add("Employer");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-    }
-
-    public void addSpinnerListener() {
-        spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(new OnItemSelectedCustom());
+        User user = new User(firstName, lastName);
+        // user.setValue(User.FIELD_UNIVERSITY_NAME, universityName);
+        updateUser(user);
     }
 
     private void updateUser(User usrObj) {
         DocumentReference docRef = db.collection("users").document(user.getUid());
-        docRef.set(usrObj)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot successfully updated!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error updating document", e);
-                    }
-                });
+        docRef.set(usrObj.getMap()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "DocumentSnapshot successfully updated!");
+                } else {
+                    Log.d(TAG, "Error updating document");
+                }
+            }
+        });
 
     }
 
     private boolean validProfileFields() {
-        spinner = (Spinner) findViewById(R.id.spinner);
-
-        final String fname = txtFname.getText().toString();
-        final String lname = txtLname.getText().toString();
-        final String usrType = spinner.getSelectedItem().toString();
+        final String fname = etFname.getText().toString();
+        final String lname = etLname.getText().toString();
 
         if (fname.isEmpty()) {
-            txtFname.setError("First name field is required.");
-            txtFname.requestFocus();
+            etFname.setError("First name field is required.");
+            etFname.requestFocus();
         } else if (lname.isEmpty()) {
-            txtLname.setError("Last name field is required.");
-            txtLname.requestFocus();
-        } else if (usrType.isEmpty()) {
-            spinner.setPrompt("Select item");
-            spinner.requestFocus();
-        }
-        else {
+            etLname.setError("Last name field is required.");
+            etLname.requestFocus();
+        } else {
             return true;
         }
 
--- android/app/src/main/java/com/example/myfair/User.java
+++ android/app/src/main/java/com/example/myfair/User.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class User {
-    public static final String TYPE_STUDENT = "student";
-    public static final String TYPE_WORKER = "worker";
+    public static final String FIELD_FIRST_NAME = "first_name";
+    public static final String FIELD_LAST_NAME = "last_name";
+    public static final String FIELD_UNIVERSITY_NAME = "university_name";
+
+    private HashMap<String, Object> map;
 
     private String type;
 
@@ -13,40 +16,21 @@ public class User {
     private String lastName;
 
     public User() {
-
+        map = new HashMap<>();
     }
 
-    public User(String type) {
-        this.type = type;
+    public User(String firstName, String lastName) {
+        map = new HashMap<>();
+        map.put(FIELD_FIRST_NAME, firstName);
+        map.put(FIELD_LAST_NAME, lastName);
     }
 
-    public User(String type, String firstName, String lastName) {
-        this.type = type;
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public HashMap<String, Object> getMap() {
+        return map;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public boolean setValue(String key, Object value) {
+        map.put(key, value);
+        return true;
     }
 }
