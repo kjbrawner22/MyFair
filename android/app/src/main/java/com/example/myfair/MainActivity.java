a776edcbeea0dae8abad3657c6883f500f529152
Taylor Ray Wilson <twilson15@crimson.ua.edu>
2019-02-12 17:22:54 -0600

    Swapper built for between fragments

--- android/app/src/main/java/com/example/myfair/MainActivity.java
+++ android/app/src/main/java/com/example/myfair/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.myfair;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,15 +18,68 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
+                    switchToHistory();
+                    break;
+                case R.id.navigation_collections:
+                    mTextMessage.setText(R.string.title_collections);
+                    switchToCollections();
+                    break;
+                case R.id.navigation_create:
+                    mTextMessage.setText(R.string.title_create);
+                    switchToCreate();
+                    break;
+                case R.id.navigation_analytics:
+                    mTextMessage.setText(R.string.title_analytics);
+                    switchToAnalytics();
+                    break;
+            }
+            return true;
+        }
+
+    };
+
+    public void switchToHistory() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragmentLayout, new CollectionsFragment()).commit();
+    }
+
+    public void switchToCollections() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragmentLayout, new CollectionsFragment()).commit();
+    }
+
+    public void switchToCreate() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragmentLayout, new CollectionsFragment()).commit();
+    }
+
+    public void switchToAnalytics() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragmentLayout, new CollectionsFragment()).commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        BottomNavigationView navBar = findViewById(R.id.navigation);
 
-        
+        mTextMessage = (TextView) findViewById(R.id.textView);
+
+        BottomNavigationView navBar = findViewById(R.id.navigation);
+        navBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
 
         Button btnSignOut = findViewById(R.id.btnSignOut);
         btnSignOut.setOnClickListener(new View.OnClickListener() {
