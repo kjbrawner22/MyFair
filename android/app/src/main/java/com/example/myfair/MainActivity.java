6c59346fdc2678bd3e58c9dc439639ade1dd947e
Taylor Ray Wilson <twilson15@crimson.ua.edu>
2019-02-11 23:54:30 -0600

    About to try fragmenting the views

--- android/app/src/main/java/com/example/myfair/MainActivity.java
+++ android/app/src/main/java/com/example/myfair/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myfair;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        BottomNavigationView navBar = findViewById(R.id.navigation);
+
+        
 
         Button btnSignOut = findViewById(R.id.btnSignOut);
         btnSignOut.setOnClickListener(new View.OnClickListener() {
