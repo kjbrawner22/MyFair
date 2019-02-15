cc394833aa68e1f039e5a818657e0f61765bc236
smartin2768 <38294715+smartin2768@users.noreply.github.com>
2019-02-15 11:03:41 -0600

    Generating and Scanning Debugging
    After a issue with opening the Generate Activity was discovered in testing. The best course of action was determined to be removing all code content and reintroducing sections of the code to determine at what point the failure is arising.

--- android/app/src/main/java/com/example/myfair/GenerateActivity.java
+++ android/app/src/main/java/com/example/myfair/GenerateActivity.java
@@ -23,73 +23,24 @@ import androidmads.library.qrgenearator.QRGSaver;
 
 public class GenerateActivity extends AppCompatActivity {
 
-    String TAG = "QRgen";
-    EditText qrInput;
-    ImageView qrImage;
-    String input;
-    Button start, save;
-    String savePath = Environment.getExternalStorageDirectory().getPath();
-    Bitmap bm;
-    QRGEncoder qrgEncoder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
 
-        qrImage = findViewById(R.id.qrImage);
-        qrInput = findViewById(R.id.qrInput);
-        start = findViewById(R.id.btnGen);
-        save = findViewById(R.id.btnSave);
 
-        start.setOnClickListener(new View.OnClickListener(){
+
+        Button goHome = findViewById(R.id.genbtnHome);
+        goHome.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                input = qrInput.getText().toString().trim();
-                if(input.length() > 0){
-                    WindowManager mngr = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    Display disp = mngr.getDefaultDisplay();
-                    Point point = new Point();
-                    disp.getSize(point);
-                    int w = point.x;
-                    int h = point.y;
-                    int smaller = w < h ? w : h;
-                    smaller = smaller * 3 / 4;
-
-                    qrgEncoder = new QRGEncoder(
-                            input, null, QRGContents.Type.TEXT, smaller);
-                    try {
-                        bm = qrgEncoder.encodeAsBitmap();
-                        qrImage.setImageBitmap(bm);
-                    } catch (WriterException e){
-                        Log.v(TAG,e.toString());
-                    }
-                }
-                else{
-                    qrInput.setError("Required");
-                }
-            }
-        });
-
-        save.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                boolean save;
-                String res;
-                try{
-                    save = QRGSaver.save(savePath, qrInput.getText().toString().trim(), bm, QRGContents.ImageType.IMAGE_JPEG);
-                    res = save ? "Image Saved" : "Image Not Saved";
-                    Toast.makeText(getApplicationContext(), res, Toast.LENGTH_LONG).show();
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(GenerateActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
 
-    public void goHome(View view){
-        Intent intent = new Intent(GenerateActivity.this , MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
 }
--- android/app/src/main/java/com/example/myfair/ScanActivity.java
+++ android/app/src/main/java/com/example/myfair/ScanActivity.java
@@ -1,7 +1,10 @@
 package com.example.myfair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class ScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        Button goHome = findViewById(R.id.scanbtnHome);
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScanActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
