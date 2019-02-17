28f19cc6ca4b8c661179efab3afaaa5abaa4f1ae
Joshua Helms <jghelms@crimson.ua.edu>
2019-02-17 03:54:55 -0600

    added pages for card creation and user collection, may move user collection later

---
+++ android/app/src/main/java/com/example/myfair/CardCreationActivity.java
@@ -0,0 +1,13 @@
+package com.example.myfair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_creation);
+    }
+}
---
+++ android/app/src/main/java/com/example/myfair/User.java
@@ -0,0 +1,4 @@
+package com.example.myfair;
+
+public class User {
+}
