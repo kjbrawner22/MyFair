a3c6cf3cb3712965a6091f11e44aeca29de19f76
smartin2768 <38294715+smartin2768@users.noreply.github.com>
2019-03-26 09:08:16 -0500

    Added User Card analytics and added the dependency for the graphing feature

---
+++ android/app/src/main/java/com/example/myfair/activities/analytics/UserCardAnalyticsActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfair.activities.analytics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfair.R;
+
+public class UserCardAnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_card_analytics);
+    }
+}
