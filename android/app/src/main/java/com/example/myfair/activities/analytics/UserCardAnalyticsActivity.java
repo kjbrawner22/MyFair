e22bfcbe48995b6a16460663a47fbf236c2254b1
smartin2768 <38294715+smartin2768@users.noreply.github.com>
2019-04-01 22:55:23 -0500

    Added Date Picking to the user card analytics option. This will progress towards picking a time frame for the graph. Additionally, a next step will include downloading the meta data list of scans and using it with the graph.

--- android/app/src/main/java/com/example/myfair/activities/analytics/UserCardAnalyticsActivity.java
+++ android/app/src/main/java/com/example/myfair/activities/analytics/UserCardAnalyticsActivity.java
@@ -1,19 +1,48 @@
 package com.example.myfair.activities.analytics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.myfair.R;
+import com.example.myfair.db.Card;
 import com.example.myfair.db.FirebaseDatabase;
-import com.google.firebase.auth.FirebaseAuth;
+import com.example.myfair.views.BusinessCardView;
+import com.example.myfair.views.GenericCardView;
+import com.example.myfair.views.UniversityCardView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.jjoe64.graphview.GraphView;
+
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class UserCardAnalyticsActivity extends AppCompatActivity {
 
     FirebaseDatabase db;
     String cardId;
     DocumentReference card;
+    LinearLayout cardSpot;
+    String TAG = "UserCardAnalyticsActivity";
+    GraphView graph;
+    DatePickerDialog fromDialog;
+    DatePickerDialog toDialog;
+    TextView fromText;
+    TextView toText;
+
+    final Calendar fromCalendar = Calendar.getInstance();
+    final Calendar toCalendar = Calendar.getInstance();
+
 
 
     @Override
@@ -33,8 +62,80 @@ public class UserCardAnalyticsActivity extends AppCompatActivity {
 
         db = new FirebaseDatabase();
         card  = db.userCards().document(cardId);
+        cardSpot = findViewById(R.id.cardPreviewLayout);
+        graph = findViewById(R.id.scansGraph);
+
+
+        fromDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                fromText.setText(newDate.getTime().toString());
+            }
+        }, fromCalendar.get(Calendar.YEAR), fromCalendar.get(Calendar.MONTH), fromCalendar.get(Calendar.DAY_OF_MONTH));
+        fromText = findViewById(R.id.fromText);
+        fromText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDialog.show();
+            }
+        });
+
+        toDialog = new DatePickerDialog(this);
+        toDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                toText.setText(newDate.getTime().toString());
+            }
+        }, toCalendar.get(Calendar.YEAR), toCalendar.get(Calendar.MONTH), toCalendar.get(Calendar.DAY_OF_MONTH));
+        toText = findViewById(R.id.toText);
+        toText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDialog.show();
+            }
+        });
+
+
+
+        makePreview(card, cardSpot);
 
 
 
     }
+
+    private void addCardView(GenericCardView v, LinearLayout listView) {
+        listView.addView(v);
+        v.setMargins();
+    }
+
+    private void makePreview(DocumentReference ref, final LinearLayout listView){
+        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    String cID = document.getId();
+                    HashMap<String,Object> map = (HashMap<String,Object>) document.getData();
+                    String type = (String) map.get(Card.FIELD_TYPE);
+                    if(type != null && type.equals(Card.VALUE_TYPE_BUSINESS)) {
+                        BusinessCardView v = new BusinessCardView(UserCardAnalyticsActivity.this, cID, map);
+                        addCardView(v, listView);
+                    }
+                    else if(type != null){
+                        UniversityCardView v = new UniversityCardView(UserCardAnalyticsActivity.this, cID, map);
+                        addCardView(v, listView);
+                    }
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
 }
