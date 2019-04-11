9c56204a6dffdb71b80f9c7a7cc17d7e141e2466
Joshua Helms <jghelms@crimson.ua.edu>
2019-04-11 12:39:40 -0500

    made new activity

---
+++ android/app/src/main/java/com/example/myfair/activities/CardViewingActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfair.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardViewingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_viewing);
+    }
+}
