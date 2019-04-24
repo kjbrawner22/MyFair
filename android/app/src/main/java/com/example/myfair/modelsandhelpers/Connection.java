cb03dcfee1a9b0136ed45e2669918b704d573b10
Taylor Ray Wilson <twilson15@crimson.ua.edu>
2019-04-23 21:15:47 -0500

    Instagram Logo made yeehaw

--- android/app/src/main/java/com/example/myfair/modelsandhelpers/Connection.java
+++ android/app/src/main/java/com/example/myfair/modelsandhelpers/Connection.java
@@ -92,7 +92,7 @@ public class Connection {
                 new Connection(R.drawable.ic_twitter, User.FIELD_TWITTER_USERNAME, "Twitter"),
                 new Connection(R.drawable.ic_github, User.FIELD_GITHUB_USERNAME, "Github"),
                 new Connection(R.drawable.ic_linked_in, User.FIELD_LINKED_IN_USERNAME, "Linked In"),
-                new Connection(R.drawable.ic_twitter, User.FIELD_INSTAGRAM_USERNAME, "Instagram")
+                new Connection(R.drawable.ic_instagram, User.FIELD_INSTAGRAM_USERNAME, "Instagram")
         };
     }
 
